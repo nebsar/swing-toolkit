0d89620f473c22a8d778eab18254d91e6a42f854
augusto.orion <augusto.orion@efa20258-e40a-62cc-d12a-0469e9ac4b1a>
2011-07-13 14:15:55 +0000

    ReflectionUtil modified. Added setter invoke capability.

--- SwingToolkit/src/ar/com/huargo/reflection/ReflectionUtil.java
+++ SwingToolkit/src/ar/com/huargo/reflection/ReflectionUtil.java
@@ -22,155 +22,172 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Augusto Recordon
  */
 public class ReflectionUtil {
-    	/**
-	 * Este m�todo retorna el valor de una property determinada
-	 * para una instancia de cualquier objeto
-	 * @param object
-	 * @param property
-	 * @return
-	 */
-	public static Object getValueOf(Object object, String property){
 
-		Object value = null;
+    /**
+     * Este m�todo retorna el valor de una property determinada
+     * para una instancia de cualquier objeto
+     * @param object
+     * @param property
+     * @return
+     */
+    public static Object getValueOf(Object object, String property) {
 
-		try {
-			value = object.getClass().getField(property).get(object);
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (NoSuchFieldException e) {
-			e.printStackTrace();
-		}
+        Object value = null;
 
-		return value;
-	}
+        try {
+            value = object.getClass().getField(property).get(object);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
 
-	public static Method getSetter(String property,Object o){
-		Method result = null;
-		Method[] methods = o.getClass().getMethods();
-		String setterName = "set"+property.substring(0,1).toUpperCase()+property.substring(1);
-		for(int i = 0; i <= methods.length; i++){
-			if(methods[i].getName().equals(setterName)){
-				result = methods[i];
-				break;
-			}
-		}
-		return result;
-	}
+        return value;
+    }
 
-	public static Object executeGetter(Object object, String property){
-		StringBuffer sb = new StringBuffer("get").append(property.substring(0,1).toUpperCase()).append(property.substring(1));
-		Object result = null;
-		try {
-			Method method = ReflectionUtil.getMethod(object.getClass(), sb.toString());
-			
-			result = method.invoke(object);
-		} catch (Exception e) {
-			
-			throw new NoSuchPropertyException();
-		}
-		return result;
-	}
+    public static Method getSetter(String property, Object o) {
+        Method result = null;
+        Method[] methods = o.getClass().getMethods();
+        String setterName = "set" + property.substring(0, 1).toUpperCase() + property.substring(1);
+        for (int i = 0; i <= methods.length; i++) {
+            if (methods[i].getName().equals(setterName)) {
+                result = methods[i];
+                break;
+            }
+        }
+        return result;
+    }
 
+    public static void executeSetter(String property, Object o, Object value, Class valueClass) {
+        try {
+            Method m = o.getClass().getMethod("set" + property.substring(0, 1).toUpperCase() + property.substring(1),valueClass);
+            m.invoke(o, value);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(ReflectionUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 
-	public static String getValueAsStringOf(Object object, String property){
-		Object obj = ReflectionUtil.getValueOf(object, property);
-		String result = null; 
-		if(obj != null){
-			result = obj.toString();
-		}
-		return result;
-	}
+    public static Object executeGetter(Object object, String property) {
+        StringBuffer sb = new StringBuffer("get").append(property.substring(0, 1).toUpperCase()).append(property.substring(1));
+        Object result = null;
+        try {
+            Method method = ReflectionUtil.getMethod(object.getClass(), sb.toString());
 
+            result = method.invoke(object);
+        } catch (Exception e) {
 
-	public static Object getInstance(Class clazz) throws InstantiationException, IllegalAccessException{
-		return clazz.newInstance();
-	}
+            throw new NoSuchPropertyException();
+        }
+        return result;
+    }
 
-	public static Object getInstance(String fullclassname){
-		Object result = null;
+    public static String getValueAsStringOf(Object object, String property) {
+        Object obj = ReflectionUtil.getValueOf(object, property);
+        String result = null;
+        if (obj != null) {
+            result = obj.toString();
+        }
+        return result;
+    }
 
-		try {
-			result = Class.forName(fullclassname).newInstance();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+    public static Object getInstance(Class clazz) throws InstantiationException, IllegalAccessException {
+        return clazz.newInstance();
+    }
 
-		}
+    public static Object getInstance(String fullclassname) {
+        Object result = null;
 
-		return result;
-	}
+        try {
+            result = Class.forName(fullclassname).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
 
-	public static Object getInstance(String fullclassname,Object param){
-		Object result = null;
-		try{
-			Constructor constructor = Class.forName(fullclassname).getConstructor(param.getClass());
-			result = constructor.newInstance(param);
-		}catch(IllegalAccessException e){
-			e.printStackTrace();
-		}catch(IllegalArgumentException e){
-			e.printStackTrace();
-		}catch(InstantiationException e){
-			e.printStackTrace();
-		}catch(InvocationTargetException e){
-			e.printStackTrace();
-		}catch(ClassNotFoundException e){
-			e.printStackTrace();
-		}catch(NoSuchMethodException e){
-			e.printStackTrace();
-		}
-		return result;
-	}
+        }
 
-	@SuppressWarnings("rawtypes")
-	public static Method getMethod(Class clazz, String name){
-		Method[] methods  = clazz.getMethods();
-		Method method = null;
-		for(Method m : methods){
-			if(m.getName().equals(name)){
-				method = m;
-				break;
-			}
-		}
-		return method;
-	}
+        return result;
+    }
 
+    public static Object getInstance(String fullclassname, Object param) {
+        Object result = null;
+        try {
+            Constructor constructor = Class.forName(fullclassname).getConstructor(param.getClass());
+            result = constructor.newInstance(param);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
-	@SuppressWarnings("rawtypes")
-	public static Field[] getFields(Class clazz){
-		return clazz.getFields();
-	}
+    @SuppressWarnings("rawtypes")
+    public static Method getMethod(Class clazz, String name) {
+        Method[] methods = clazz.getMethods();
+        Method method = null;
+        for (Method m : methods) {
+            if (m.getName().equals(name)) {
+                method = m;
+                break;
+            }
+        }
+        return method;
+    }
 
-	@SuppressWarnings("rawtypes")
-	public static Object getFieldValue(Class clazz, String fieldName) throws IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException{
-		return clazz.getField(fieldName).get(clazz);
-	}
+    @SuppressWarnings("rawtypes")
+    public static Field[] getFields(Class clazz) {
+        return clazz.getFields();
+    }
 
-	@SuppressWarnings("rawtypes")
-	public static void setFieldValue(Class clazz,String fieldName,Object value) throws IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException{
-		clazz.getField(fieldName).set(clazz, value);
-	}
+    @SuppressWarnings("rawtypes")
+    public static Object getFieldValue(Class clazz, String fieldName) throws IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException {
+        return clazz.getField(fieldName).get(clazz);
+    }
 
-	public static boolean isInstanceOf(Class clazz, Object o){
-		Class oClass = o.getClass();
-		boolean result = oClass.equals(clazz);
-		while((!result) && (oClass != Object.class)){
-			oClass = oClass.getSuperclass();
-			result = oClass.equals(clazz);
-		}
-		return result;
-	}
-    
+    @SuppressWarnings("rawtypes")
+    public static void setFieldValue(Class clazz, String fieldName, Object value) throws IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException {
+        clazz.getField(fieldName).set(clazz, value);
+    }
+
+    public static boolean isInstanceOf(Class clazz, Object o) {
+        Class oClass = o.getClass();
+        boolean result = oClass.equals(clazz);
+        while ((!result) && (oClass != Object.class)) {
+            oClass = oClass.getSuperclass();
+            result = oClass.equals(clazz);
+        }
+        return result;
+    }
 }
