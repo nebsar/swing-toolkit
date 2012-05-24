7f855760d7847fa9de89ae660d130f036f53c29c
augusto.orion <augusto.orion@efa20258-e40a-62cc-d12a-0469e9ac4b1a>
2012-05-24 16:48:33 +0000

    Several modifications to different factories.

---
+++ SwingToolkit/src/ar/com/nasel/annotation/processor/LengthAnnotationProcessor.java
@@ -0,0 +1,52 @@
+/**
+Copyright (C) 2011  Augusto Recordon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+package ar.com.nasel.annotation.processor;
+
+import ar.com.nasel.annotation.annotation.Length;
+import ar.com.nasel.exception.annotation.InvalidLengthForFieldException;
+import ar.com.nasel.exception.annotation.ValidationFailedException;
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Augusto Recordon
+ */
+public class LengthAnnotationProcessor extends AnnotationProcessorImpl {
+
+    @Override
+    protected void processField(Field f, Object o) throws ValidationFailedException {
+        Length lengthAnnotation = f.getAnnotation(Length.class);
+        if (lengthAnnotation != null) {
+            String value;
+            try {
+                value = f.get(o).toString();
+
+                int length = value.length();
+                if (!((length >= lengthAnnotation.min()) && (length <= lengthAnnotation.max()))) {
+                    throw new InvalidLengthForFieldException();
+                }
+            } catch (IllegalArgumentException ex) {
+                Logger.getLogger(LengthAnnotationProcessor.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                Logger.getLogger(LengthAnnotationProcessor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
